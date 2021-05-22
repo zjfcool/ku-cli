b7707a5c531bf4d04cb372e7539a1bccc8ff6cb5
zjfcool <2568696196@qq.com>
2021-05-22 11:41:35 +0800

    update pkg;add yarn judgment condition;engines:node;

--- utils/env.js
+++ utils/env.js
@@ -1,8 +1,18 @@
 import fs from 'fs'
 import path from 'path'
+import { execSync } from 'child_process'
 
 const hasYarnProject = (cwd = process.cwd()) => fs.existsSync(path.resolve(cwd, 'yarn.lock'))
+const hasYarn = () => {
+    try {
+        execSync('yarn --version', { stdio: 'ignore' })
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
 
 export {
-    hasYarnProject
+    hasYarnProject,
+    hasYarn
 }
